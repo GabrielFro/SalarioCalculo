3d20a0b483987173ec2ca9dc0bb4062b46fff11d
Gabriel Silva <gabrielkko7@gmail.com>
2022-11-04 22:15:04 -0300

    Initial commit

---
+++ app/src/main/java/br/ulbra/salariocalculo/MainActivity.java
@@ -0,0 +1,47 @@
+package br.ulbra.salariocalculo;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+
+public class MainActivity extends AppCompatActivity {
+    RadioGroup rgopcoes;
+    Button btcalcular;
+    EditText edsalario;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        edsalario = (EditText) findViewById(R.id.edsalario);
+        rgopcoes = (RadioGroup) findViewById(R.id.rgopcoes);
+        btcalcular = (Button) findViewById(R.id.btcalcular);
+        btcalcular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                double salario = Double.parseDouble(edsalario.getText().toString());
+                int op = rgopcoes.getCheckedRadioButtonId();
+                double novo_salario = 0;
+
+                if (op == R.id.radioButton1)
+                    novo_salario = salario + (salario * 0.4);
+                else if (op == R.id.radioButton2)
+                    novo_salario = salario + (salario * 0.45);
+                else
+                    novo_salario = salario + (salario * 0.5);
+
+                AlertDialog.Builder dialogo = new AlertDialog.Builder(MainActivity.this);
+                dialogo.setTitle("Novo Salário");
+                dialogo.setMessage("Seu novo salário é: R$"+String.valueOf(novo_salario));
+                dialogo.setNeutralButton("Ok", null);
+                dialogo.show();
+            }
+        });
+    }
+}
